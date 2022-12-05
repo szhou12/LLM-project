14bb53e0d67c77dcb7174aaca5eb8f1707bdf4e8
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-12-05 04:49:17 -0600

    streamlit app, ethical questions, model jupyter

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import argparse
+import time
+
+def run():
+    st.markdown(
+        """
+        ## GPT Model
+        """
+    )
+
+    st.sidebar.subheader("Parameters")
+
+    generate_max_len = st.sidebar.number_input("generate_max_len", min_value=0, max_value=512, value=32, step=1)
+    top_k = st.sidebar.slider("top_k", min_value=0, max_value=10, value=3, step=1)
+    top_p = st.sidebar.number_input("top_p", min_value=0.0, max_value=1.0, value=.95, step=.01)
+    temperature = st.sidebar.number_input("temperature", min_value=0.0, max_value=100.0, value=1.0, step=.1)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--generate_max_len', default=generate_max_len, type=int, help='max length to generate')
+    parser.add_argument('--top_k', default=top_k, type=float, help='')
+    parser.add_argument('--top_p', default=top_p, type=float, help='')
+    parser.add_argument('--temperature', default=temperature, type=float, help='')
+    parser.add_argument('--max_len', default=512, type=int, help='')
+    args = parser.parse_args()
+
+    title = st.text_area('Enter Title:', max_chars=512)
+    context = st.text_area('Enter Context:', max_chars=512)
+
+    if st.button('Submit'):
+        start_message = st.empty()
+        start_message.write('Processing...')
+        start_time = time.time()
+        # Call pre-trained model
+        result = 'placeholder'
+        end_time = time.time()
+        start_message.write(f'Finished in {end_time-start_time}s')
+        st.text_area('Result:', value=result, key=None)
+    else:
+        st.stop()
+
+if __name__ == '__main__':
+    try:
+        run()
+    except KeyboardInterrupt:
+        pass
